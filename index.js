dec5c0549a26830e72bbfe9e390233e4dd8705dd
dalk <admin@thecool.link>
2022-03-12 20:24:25 -0600

    f

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const dbmanager = require('./file.js')
-
-const NS =  dbmanager()
-
-const WS =  `r/${dbmanager()}`
-
-const SubLink =  `https://www.reddit.com/r/${dbmanager()}`
-
 exports.NoSlash = function() {
+    const dbmanager = require('./file.js')
+    const NS =  dbmanager()
     return NS
         }
 exports.WithSlash = function() {
+    const dbmanager = require('./file.js')
+    const WS =  `r/${dbmanager()}`
     return WS
         }
 exports.Link = function() {
+    const dbmanager = require('./file.js')
+    const SubLink =  `https://www.reddit.com/r/${dbmanager()}`
     return SubLink
         }
